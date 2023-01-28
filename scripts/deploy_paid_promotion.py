a9bf983715ff91c4fec310ff1363ee81b48eb81c
Noro <noromeb85@gmail.com>
2023-01-28 16:51:22 +0100

    added transferAndCall link method and

--- scripts/deploy_paid_promotion.py
+++ scripts/deploy_paid_promotion.py
@@ -20,6 +20,11 @@ def deploy():
 
 def request():
     paid_promotion = PaidPromotion[-1]
-    request_tx = paid_promotion.getViwesCount(
-        {"from": account, "priority_fee": "10 gwei"}
+    link = Contract.from_explorer("0x326C977E6efc84E512bB9C30f76E30c160eD06FB")
+    api_url = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=G7KNmW9a75Y&key=AIzaSyBJN4rbtGCZiMgSegJ2W8YnPC_gb0ynj3Q"
+    request_tx = link.transferAndCall(
+        paid_promotion,
+        Web3.toWei(0.1, "ether"),
+        paid_promotion.requestViewsCountData.encode_input(api_url),
+        {"from": account, "priority_fee": "10 gwei"},
     )
