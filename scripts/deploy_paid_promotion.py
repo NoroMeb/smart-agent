614f48f5636ae03d8c980d77acb765d6e98664f1
Noro <noromeb85@gmail.com>
2023-01-28 16:52:33 +0100

    some verifications

--- scripts/deploy_paid_promotion.py
+++ scripts/deploy_paid_promotion.py
@@ -1,5 +1,6 @@
-from brownie import PaidPromotion, APIConsumer
+from brownie import PaidPromotion, APIConsumer, Contract, config, network
 from scripts.utils import get_account
+from web3 import Web3
 
 
 account = get_account()
@@ -7,14 +8,22 @@ account = get_account()
 
 def main():
     # deploy()
-    request()
+    # request()
+    view()
 
 
 def deploy():
-    api_consumer = APIConsumer[-1]
+    link_token = config["networks"][network.show_active()]["link_token"]
+    chainlink_oracle = config["networks"][network.show_active()]["chainlink_oracle"]
     api_url = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=G7KNmW9a75Y&key=AIzaSyBJN4rbtGCZiMgSegJ2W8YnPC_gb0ynj3Q"
+    job_id = config["networks"][network.show_active()]["job_id"]
+
     paid_promotion = PaidPromotion.deploy(
-        api_url, api_consumer, {"from": account, "priority_fee": "10 gwei"}
+        link_token,
+        chainlink_oracle,
+        job_id,
+        api_url,
+        {"from": account, "priority_fee": "10 gwei"},
     )
 
 
@@ -28,3 +37,10 @@ def request():
         paid_promotion.requestViewsCountData.encode_input(api_url),
         {"from": account, "priority_fee": "10 gwei"},
     )
+
+
+def view():
+    paid_promotion = PaidPromotion[-1]
+    print("========================")
+    print(paid_promotion.viewsCount())
+    print("========================")
