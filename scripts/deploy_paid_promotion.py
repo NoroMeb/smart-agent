ce7b57510b9cf05aed4e11389122c7f2cf2a250f
Noro <noromeb85@gmail.com>
2023-02-01 16:18:30 +0100

    format api url

--- scripts/deploy_paid_promotion.py
+++ scripts/deploy_paid_promotion.py
@@ -1,47 +1,60 @@
-from brownie import PaidPromotion, APIConsumer, Contract, config, network
+from brownie import PaidPromotion, Contract, config, network
 from scripts.utils import get_account
 from web3 import Web3
 
 
 account = get_account()
+api_key = config["networks"][network.show_active()]["api_key"]
 
 
 def main():
     # deploy()
-    request_views_count()
-    # view()
+    # start_a_collab("bY2yvESoWAs")
+    # start_a_collab("9kP12wIp3wA")
+    # request_views_count(1)
+    view()
 
 
 def deploy():
     link_token = config["networks"][network.show_active()]["link_token"]
     chainlink_oracle = config["networks"][network.show_active()]["chainlink_oracle"]
-    api_url = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=G7KNmW9a75Y&key=AIzaSyBJN4rbtGCZiMgSegJ2W8YnPC_gb0ynj3Q"
     job_id = config["networks"][network.show_active()]["job_id"]
+
+    paid_promotion = PaidPromotion.deploy(
+        link_token,
+        chainlink_oracle,
+        job_id,
+        {"from": account, "priority_fee": "10 gwei"},
+    )
+
+
+def start_a_collab(video_id):
+    paid_promotion = PaidPromotion[-1]
     promoter = "0x108A176896bAD4E05b5C4BE738839fDC4238c526"
     client = "0x8FB5F1947F1325072307c87e987162f4Cdf823aC"
-    end_timestamp = 1680335012
+    api_url = f"https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id={video_id}&key={api_key}"
+    end_time_stamp = 1680348420
     level = 10000
     amount = Web3.toWei(0.8, "ether")
 
-    paid_promotion = PaidPromotion.deploy(
-        api_url,
+    start_collab_tx = paid_promotion.startACollab(
         promoter,
         client,
-        end_timestamp,
+        api_url,
+        end_time_stamp,
         level,
         amount,
         {"from": account, "priority_fee": "10 gwei"},
     )
 
 
-def request_views_count():
+def request_views_count(id):
     paid_promotion = PaidPromotion[-1]
     link = Contract.from_explorer("0x326C977E6efc84E512bB9C30f76E30c160eD06FB")
-    api_url = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=G7KNmW9a75Y&key=AIzaSyBJN4rbtGCZiMgSegJ2W8YnPC_gb0ynj3Q"
     request_tx = link.transferAndCall(
         paid_promotion,
         Web3.toWei(0.1, "ether"),
-        paid_promotion.requestViewsCountData.encode_input(api_url),
+        paid_promotion.requestViewsCountData.encode_input(id),
         {"from": account, "priority_fee": "10 gwei"},
     )
 
@@ -49,5 +62,5 @@ def request_views_count():
 def view():
     paid_promotion = PaidPromotion[-1]
     print("========================")
-    print(paid_promotion.viewsCount())
+    print(paid_promotion.collabById(0))
     print("========================")
