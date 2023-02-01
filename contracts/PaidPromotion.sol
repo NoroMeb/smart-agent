33940450c471b549a5758de67b92c3667593fd33
Noro <noromeb85@gmail.com>
2023-02-01 16:19:15 +0100

    restructured PaidPromotion

--- contracts/PaidPromotion.sol
+++ contracts/PaidPromotion.sol
@@ -1,37 +1,103 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "./data/APIConsumer.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
-contract PaidPromotion is APIConsumer {
-    address payable public promoter;
-    address public client;
-    mapping(uint256 => uint256) levelToAmount;
-    uint256 public promoterOwing;
-    uint256 public endTimestamp;
+contract PaidPromotion is ChainlinkClient, ConfirmedOwner {
+    using Chainlink for Chainlink.Request;
+
+    uint256 public viewsCount;
+    bytes private jobId;
+    uint256 internal fee;
+
+    event RequestViewsCount(bytes32 indexed requestId, uint256 viewsCount);
+
+    struct Collab {
+        address promoter;
+        address client;
+        string apiUrl;
+        uint256 endTimestamp;
+        uint256 level;
+        uint256 amount;
+        uint256 promoterBalance;
+        uint256 clientBalance;
+    }
+
+    mapping(uint256 => Collab) public collabById;
+    uint256 id;
 
     constructor(
-        string memory _apiUrl,
-        address payable _promoter,
+        address _chainlinkToken,
+        address _chainlinkOracle,
+        string memory _jobId
+    ) ConfirmedOwner(msg.sender) {
+        setChainlinkToken(_chainlinkToken);
+        setChainlinkOracle(_chainlinkOracle);
+        jobId = bytes(_jobId);
+        fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job) .
+        id = 0;
+    }
+
+    // modifier onlyPromoter() {
+    //     require(msg.sender == promoter, "Only promoter can call this function");
+    //     _;
+    // }
+
+    // modifier onlyClient() {
+    //     require(msg.sender == client, "Only client can call this function");
+    //     _;
+    // }
+
+    function startACollab(
+        address _promoter,
         address _client,
+        string memory _apiUrl,
         uint256 _endTimestamp,
         uint256 _level,
         uint256 _amount
-    ) payable APIConsumer() {
-        promoter = _promoter;
-        client = _client;
-        endTimestamp = _endTimestamp;
+    ) external payable {
+        Collab memory collab = Collab(
+            _promoter,
+            _client,
+            _apiUrl,
+            _endTimestamp,
+            _level,
+            _amount,
+            0,
+            msg.value
+        );
+        collabById[id] = collab;
+        id = id + 1;
     }
 
-    modifier onlyPromoter() {
-        require(msg.sender == promoter, "Only promoter can call this function");
-        _;
+    function requestViewsCountData(uint256 _id)
+        public
+        returns (bytes32 requestId)
+    {
+        string memory apiUrl = collabById[_id].apiUrl;
+
+        Chainlink.Request memory req = buildChainlinkRequest(
+            bytes32(jobId),
+            address(this),
+            this.fulfill.selector
+        );
+
+        // Set the URL to perform the GET request on
+        req.add("get", apiUrl);
+
+        req.add("path", "items,0,statistics,viewCount");
+
+        // Sends the request
+        return sendChainlinkRequest(req, fee);
     }
 
-    modifier onlyClient() {
-        require(msg.sender == client, "Only client can call this function");
-        _;
+    function fulfill(bytes32 _requestId, uint256 _viewsCount)
+        public
+        recordChainlinkFulfillment(_requestId)
+    {
+        emit RequestViewsCount(_requestId, _viewsCount);
+        viewsCount = _viewsCount;
     }
 
     function onTokenTransfer(
@@ -40,13 +106,10 @@ contract PaidPromotion is APIConsumer {
         bytes calldata _data
     ) public {
         require(_fee >= fee, "NOT ENOUGH FUNDS");
-
         bytes memory data = _data[4:];
-
-        string memory apiUrl = abi.decode(data, (string));
-
+        uint256 apiUrl = abi.decode(data, (uint256));
         requestViewsCountData(apiUrl);
-        // (bool success, ) = address(this).call(_data);
-        // require(success, "NOT SUCCESS");
     }
+
+    function endCollab(uint256 _id) external {}
 }
