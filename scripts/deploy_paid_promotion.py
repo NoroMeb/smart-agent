181b7714bc0540f4c8c80fd8a37ad6c61b7f816c
Noro <noromeb85@gmail.com>
2023-02-02 11:51:00 +0100

    edited script

--- scripts/deploy_paid_promotion.py
+++ scripts/deploy_paid_promotion.py
@@ -1,18 +1,20 @@
-from brownie import PaidPromotion, Contract, config, network
+from brownie import PaidPromotion, Contract, config, network, accounts
 from scripts.utils import get_account
 from web3 import Web3
 
 
 account = get_account()
 api_key = config["networks"][network.show_active()]["api_key"]
+account_2 = accounts.add(config["wallets"]["from_key_2"])
 
 
 def main():
     # deploy()
     # start_a_collab("bY2yvESoWAs")
-    # start_a_collab("9kP12wIp3wA")
-    # request_views_count(1)
+    # withdraw_ether(0)
     view()
+    # end_collab(0)
+    # view()
 
 
 def deploy():
@@ -33,34 +35,39 @@ def start_a_collab(video_id):
     promoter = "0x108A176896bAD4E05b5C4BE738839fDC4238c526"
     client = "0x8FB5F1947F1325072307c87e987162f4Cdf823aC"
     api_url = f"https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id={video_id}&key={api_key}"
-    end_time_stamp = 1680348420
+    end_time_stamp = 1675329000
     level = 10000
-    amount = Web3.toWei(0.8, "ether")
+    amount = Web3.toWei(0.01, "ether")
 
     start_collab_tx = paid_promotion.startACollab(
-        promoter,
         client,
+        promoter,
         api_url,
         end_time_stamp,
         level,
         amount,
-        {"from": account, "priority_fee": "10 gwei"},
+        {"from": account, "priority_fee": "10 gwei", "value": amount},
     )
 
 
-def request_views_count(id):
+def withdraw_ether(id):
     paid_promotion = PaidPromotion[-1]
     link = Contract.from_explorer("0x326C977E6efc84E512bB9C30f76E30c160eD06FB")
     request_tx = link.transferAndCall(
         paid_promotion,
         Web3.toWei(0.1, "ether"),
-        paid_promotion.requestViewsCountData.encode_input(id),
+        paid_promotion.withdrawEther.encode_input(id),
         {"from": account, "priority_fee": "10 gwei"},
     )
+
+
+def end_collab(id):
+    paid_promotion = PaidPromotion[-1]
+    end_tx = paid_promotion.endCollab(id, {"from": account, "priority_fee": "10 gwei"})
 
 
 def view():
     paid_promotion = PaidPromotion[-1]
     print("========================")
-    print(paid_promotion.collabById(0))
+    print(paid_promotion.viewsCount())
     print("========================")
