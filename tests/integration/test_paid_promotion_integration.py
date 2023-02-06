cfc8d7673d15faea5044c957c9eaf8aa17154624
Noro <noromeb85@gmail.com>
2023-02-06 13:16:52 +0100

    updated integration test

--- tests/integration/test_paid_promotion_integration.py
+++ tests/integration/test_paid_promotion_integration.py
@@ -1,24 +1,27 @@
+import time
 from web3 import Web3
 from scripts.utils import get_account
-from brownie import accounts, config, MockPaidPromotion, network
+from brownie import accounts, config, MockPaidPromotion, network, reverts, Contract
 
 
-def test_paid_promotion_integration():
+def test_paid_promotion_integration(skip_local_testing):
     # arrange
     account = get_account()
     second_account = accounts.add(config["wallets"]["from_key_2"])
     link = config["networks"][network.show_active()]["link_token"]
+    link_contract = Contract.from_explorer(link)
     chainlink_oracle = config["networks"][network.show_active()]["chainlink_oracle"]
     job_id = config["networks"][network.show_active()]["job_id"]
     priority_fee = "10 gwei"
     promoter = account
+    promoter_initial_balance = promoter.balance()
     client = second_account
-    client_balance = Web3.toWei(2, "ether")
+    client_balance = Web3.toWei(1, "ether")
     video_id = "l4uCnAWj-6I"
     api_key = config["networks"][network.show_active()]["api_key"]
     api_url = f"https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id={video_id}&key={api_key}"
     end_time_stamp = 2653911765
-    amount = Web3.toWei(0.0001, "ether")
+    amount = Web3.toWei(0.00001, "ether")
 
     # act
     paid_promotion = MockPaidPromotion.deploy(
@@ -31,23 +34,28 @@ def test_paid_promotion_integration():
         api_url,
         end_time_stamp,
         amount,
-        {"from": account, "priority_fee": priority_fee, "value": client_balance},
+        {"from": client, "priority_fee": priority_fee, "value": client_balance},
     )
 
-    request_tx = link.transferAndCall(
+    request_tx = link_contract.transferAndCall(
         paid_promotion,
         Web3.toWei(0.1, "ether"),
-        paid_promotion.withdrawEther.encode_input(id),
-        {"from": account, "priority_fee": priority_fee},
+        paid_promotion.withdrawEther.encode_input(0),
+        {"from": promoter},
     )
+
+    event_filter = paid_promotion.events.RequestViewsCount.createFilter(
+        fromBlock="latest"
+    )
+    while True:
+        events = event_filter.get_new_entries()
+        if len(events) > 0:
+            break
 
     # assert
     assert paid_promotion.id() == 1
-    assert paid_promotion.collabById(0) == (
-        promoter,
-        client,
-        api_url,
-        end_time_stamp,
-        amount,
-        client_balance,
+    assert promoter.balance() > promoter_initial_balance
+
+    assert paid_promotion.collabById(0)[5] == client_balance - (
+        paid_promotion.collabById(0)[6] * amount
     )
